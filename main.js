ae433e3261f7d9c68caf74419e3fe6d4ee655115
2sl2m <Eslam.Elgebaly1996@gmail.com>
2023-12-01 14:55:15 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,133 @@
+// window.alert("Hallo")
+// document.getElementById("demo").innerHTML="HTML,CSS AND POOTSTRAP AND JS"
+// console.log(10)
+
+
+
+
+var x = window .prompt ( "Entar your name")
+document .getElementById ("demo") .innerHTML= "wellcom"+ x
+
+
+
+// var num1 = Number(window.prompt ("Enter Number 1"))
+// var num2 = Number(window.prompt ("Enter Number 2"))
+
+// var sum =num1+num2
+
+// document .getElementById("demo").innerHTML="the Result:" +sum 
+
+
+
+// var userName = window.prompt("Enter your Name : ")
+
+// var fullName = "wellcom " + userName
+
+// document.getElementById("demo").innerHTML= fullName
+
+
+// var userName= "Eslam "
+
+//  userName += "khalifa "
+//  userName += "Elgebaly "
+//  document.getElementById("demo").innerHTML= userName
+
+// 1-  enteractions  
+//   2- data bilding
+//   3- validation
+
+
+// var x = 10
+//  x =20 
+//  x ++
+//  ++ x 
+//  x += 2 
+//  x -= 3
+//  x = 2
+//  x += 2
+
+//  console.log(x);
+
+
+
+// var role = window.prompt("enter your role :")
+
+
+// if (role == "admin" || role == "eslam") {
+
+//     console.log("go to admin page");
+// }
+
+// else if (role == "moderetor"){
+
+//     console.log("go to moderetor page");
+// }
+
+//  else {
+//     console.log("go to user page");
+//  }
+
+
+// var num1 = Number(window.prompt("Enter Number 1"))
+// var op = window.prompt("Enter operetor :");
+// var num2 = Number(window.prompt("Enter Number 2"))
+
+// switch (op){
+//  case "+":
+//       console.log(num1+num2);
+//         break;
+//  case "/":
+//         console.log(num1/num2);
+//         break;
+//  case "-":
+//             console.log(num1-num2);
+//         break;
+//  case "*":
+//                 console.log(num1*num2);
+//                 break;
+// }
+
+// for loop
+
+// for ( var i=0 ; i < 10 ; i++ ){
+//        console.log("ok" + i );
+// }
+
+
+// var str= "<il>eslam elgebaly </il>";
+
+// for ( var i = 0 ; i < 10 ; i++ ){
+i
+//     str +=  "<il>eslam elgebaly</il>";
+// }
+
+// document.getElementById("demo").innerHTML= str
+
+
+// for ( var i=0 ; i < 10 ; i++ ){
+//        console.log("ok" + i );
+// }
+
+
+// var str  =""
+
+// for ( var i = 1950 ; i < 2024 ; i++ ){
+
+//     str +=  "<option>" + i + "</option>"
+
+// }
+
+// document.getElementById("demo").innerHTML = str
+
+
+
+
+// var str  =""
+
+// for ( var i = 2023 ; i > 1950; i-- ){
+
+//     str +=  "<option>" + i + "</option>"
+
+// }
+
+// document.getElementById("demo").innerHTML = str
